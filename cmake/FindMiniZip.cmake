ff364b17c71ee0e8fcb1fc32ea78e8704e6e8ec0
Vollstrecker <werner@vollstreckernet.de>
2024-09-26 18:40:12 +0200

    Migrate minizip from find_super to fetchconentent

--- cmake/FindMiniZip.cmake
+++ cmake/FindMiniZip.cmake
@@ -1,34 +1,12 @@
-# Find the Minizip library
-# Defines:
+include(FetchContent)
 
-#  MINIZIP_INCLUDE_DIR - minizip include directory
-#  MINIZIP_LIBRARY     - minizip library file
-#  MINIZIP_FOUND       - TRUE if minizip is found
+FetchContent_Declare (minizip
+    GIT_REPOSITORY  https://github.com/zlib-ng/minizip-ng.git
+    GIT_TAG         4.0.7
+)
 
-if (MINIZIP_INCLUDE_DIR)
- #check cache 
-  set(MINIZIP_FIND_QUIETLY TRUE)
-endif ()
+find_package(minizip)
 
-if (NOT MINIZIP_INCLUDE_DIR)
-    find_path(MINIZIP_INCLUDE_DIR NAMES unzip.h PATH_SUFFIXES minizip)
-    set(MINIZIP_INCLUDE_DIR ${MINIZIP_INCLUDE_DIR}/minizip CACHE PATH "minizip includes")
-endif ()
-
-find_library(MINIZIP_LIBRARIES NAMES minizip)
-
-if (MINIZIP_INCLUDE_DIR AND MINIZIP_LIBRARIES)
-  set(MINIZIP_FOUND TRUE)
-endif ()
-
-if (MINIZIP_FOUND)
-   if (NOT MINIZIP_FIND_QUIETLY)
-      message(STATUS "Found Minizip library: ${MINIZIP_LIBRARY}")
-   endif ()
-else ()
-   if (NOT MINIZIP_FIND_QUIETLY)
-      message(FATAL_ERROR "Could NOT find Minizip library")
-    else ()
-      message(STATUS "Could NOT find Minizip library")
-    endif ()
-endif ()
+if (NOT MINIZIP_FOUND)
+    FetchContent_MakeAvailable (minizip)
+endif (NOT MINIZIP_FOUND)
