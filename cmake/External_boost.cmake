5d4bd6794c3afd155ae07ea5ffa64efaa58ef2af
Vollstrecker <werner@vollstreckernet.de>
2024-09-26 18:36:37 +0200

    Use fetchcontent to retrieve boost_smartptr with all deps.

--- cmake/External_boost.cmake
+++ cmake/External_boost.cmake
@@ -1,10 +1,58 @@
-ExternalProject_Add(BOOST
-  PREFIX BOOST
-  URL "http://sourceforge.net/projects/boost/files/boost-binaries/1.50.0/boost_1_50_headers.zip/download"
-  URL_MD5 1605dc6085cb2dc778ef5ab6c0e59083
-  SOURCE_DIR ${INSTALL_DIR}/include/boost
-  CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
-  INSTALL_COMMAND "" )
+include(FetchContent)
 
-set(Boost_INCLUDE_DIRS ${INSTALL_DIR}/include)
+FetchContent_Declare (boost_assert
+    GIT_REPOSITORY https://github.com/boostorg/assert.git
+    GIT_TAG        boost-1.86.0
+)
+
+
+FetchContent_Declare (boost_config
+    GIT_REPOSITORY https://github.com/boostorg/config.git
+    GIT_TAG        boost-1.86.0
+)
+
+
+FetchContent_Declare (boost_core
+    GIT_REPOSITORY https://github.com/boostorg/core.git
+    GIT_TAG        boost-1.86.0
+)
+
+
+FetchContent_Declare (boost_move
+    GIT_REPOSITORY https://github.com/boostorg/move.git
+    GIT_TAG        boost-1.86.0
+)
+
+
+FetchContent_Declare (boost_static_assert
+    GIT_REPOSITORY https://github.com/boostorg/static_assert.git
+    GIT_TAG        boost-1.86.0
+)
+
+
+FetchContent_Declare (boost_throw_exception
+    GIT_REPOSITORY https://github.com/boostorg/throw_exception.git
+    GIT_TAG        boost-1.86.0
+)
+
+
+FetchContent_Declare (boost_type_traits
+    GIT_REPOSITORY https://github.com/boostorg/type_traits.git
+    GIT_TAG        boost-1.86.0
+)
+
+FetchContent_Declare (boost_smart_ptr
+    GIT_REPOSITORY https://github.com/boostorg/smart_ptr.git
+    GIT_TAG        boost-1.86.0
+)
+
+FetchContent_MakeAvailable(
+    boost_assert
+    boost_config
+    boost_core
+    boost_move
+    boost_static_assert
+    boost_throw_exception
+    boost_type_traits
+    boost_smart_ptr
+)
