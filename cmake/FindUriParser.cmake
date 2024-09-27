b0a30334862aa50cde82f21ecf6f0fd820511065
Vollstrecker <werner@vollstreckernet.de>
2024-09-27 09:41:16 +0200

    set vars in cache to silence warnings

--- cmake/FindUriParser.cmake
+++ cmake/FindUriParser.cmake
@@ -8,8 +8,8 @@ FetchContent_Declare (uriparser
 find_package(uriparser CONFIG)
 
 if (NOT URIPARSER_FOUND)
-    set (URIPARSER_BUILD_TESTS OFF)
-    set (URIPARSER_BUILD_DOCS OFF)
+    set (URIPARSER_BUILD_TESTS CACHE BOOL OFF)
+    set (URIPARSER_BUILD_DOCS CACHE BOOL OFF)
     FetchContent_MakeAvailable (uriparser)
     #Alias can be removed when https://github.com/uriparser/uriparser/pull/197 is merged
     add_library (uriparser::uriparser ALIAS uriparser)
