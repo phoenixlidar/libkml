bf5e24d3e9c020f2669d34db928c0b1d4768b568
Vollstrecker <werner@vollstreckernet.de>
2024-10-20 02:04:58 +0200

    Switched to full boost download to get installation

--- cmake/External_boost.cmake
+++ cmake/External_boost.cmake
@@ -1,52 +1,17 @@
 include(FetchContent)
 
-FetchContent_Declare (boost_assert
-    GIT_REPOSITORY https://github.com/boostorg/assert.git
-    GIT_TAG        boost-1.86.0
+FetchContent_Declare (boost
+        GIT_REPOSITORY  https://github.com/boostorg/boost.git
+        GIT_TAG         boost-1.86.0
 )
 
-
-FetchContent_Declare (boost_config
-    GIT_REPOSITORY https://github.com/boostorg/config.git
-    GIT_TAG        boost-1.86.0
-)
-
-
-FetchContent_Declare (boost_core
-    GIT_REPOSITORY https://github.com/boostorg/core.git
-    GIT_TAG        boost-1.86.0
-)
-
-
-FetchContent_Declare (boost_move
-    GIT_REPOSITORY https://github.com/boostorg/move.git
-    GIT_TAG        boost-1.86.0
-)
-
-
-FetchContent_Declare (boost_static_assert
-    GIT_REPOSITORY https://github.com/boostorg/static_assert.git
-    GIT_TAG        boost-1.86.0
-)
-
-
-FetchContent_Declare (boost_throw_exception
-    GIT_REPOSITORY https://github.com/boostorg/throw_exception.git
-    GIT_TAG        boost-1.86.0
-)
-
-
-FetchContent_Declare (boost_type_traits
-    GIT_REPOSITORY https://github.com/boostorg/type_traits.git
-    GIT_TAG        boost-1.86.0
-)
-
-FetchContent_Declare (boost_smart_ptr
-    GIT_REPOSITORY https://github.com/boostorg/smart_ptr.git
-    GIT_TAG        boost-1.86.0
-)
+set (BOOST_INCLUDE_LIBRARIES "smart_ptr")
 
 FetchContent_MakeAvailable(
+    boost
+)
+
+boost_install(TARGETS
     boost_assert
     boost_config
     boost_core
