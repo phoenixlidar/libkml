ca03e630d70729e1057ff4ab6364303371ffc414
Vollstrecker <werner@vollstreckernet.de>
2024-10-30 08:58:18 +0100

    make finding and downloading boost more convenient

--- cmake/External_boost.cmake
+++ cmake/External_boost.cmake
@@ -3,6 +3,16 @@ include(FetchContent)
 FetchContent_Declare (boost
         GIT_REPOSITORY  https://github.com/boostorg/boost.git
         GIT_TAG         boost-1.86.0
+	GIT_SUBMODULES	libs/assert
+			libs/config
+			libs/core
+			libs/move
+			libs/smart_ptr
+			libs/static_assert
+			libs/throw_exception
+			libs/type_traits
+			tools/cmake
+			libs/headers
 )
 
 set (BOOST_INCLUDE_LIBRARIES "smart_ptr")
